470d2a25fdd6798d7812de25537f028666be8fff
Mateus <f.mateusaraujo20@gmail.com>
2022-09-08 11:31:52 -0300

    erro line 49

--- arq/Listas/User.c
+++ arq/Listas/User.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "User.h"
-#define tam 30
+#define tam 10
 
 typedef struct Node Node;
 
@@ -41,12 +41,13 @@ void User_free(User *u) {
 	}
 }
 
-void User_insert(User* u, char login[tam], int password) {
+void User_insert(User *u, char login[], int password) {
 	Node *new = NULL;
 	if (u)
 	{
 		new = malloc(sizeof(Node));
 		new -> login[tam] = login[tam];
+		printf("\n%s\n", new->login);
         new -> password = password;
 		new -> next = u -> first;
 		u -> first = new;
@@ -54,9 +55,21 @@ void User_insert(User* u, char login[tam], int password) {
 	}
 }
 
-//LISTAR
+void List_print(User *u) {
+	Node *n;
+	if (u)
+	{
+		n = u -> first;
 
-int List_remove(User* u, char login[tam], int password) {
+		while (n) {
+			printf("login: %s || password: %d\n", n->login, n->password);
+			n = n->next;
+		} //quando n apontar pra NULL, acaba a repetição
+		printf("\n");
+	}
+}
+
+int List_remove(User* u, char login[tam]) {
 	Node *n, *ant = NULL;
 	if (u)
 	{
@@ -77,4 +90,52 @@ int List_remove(User* u, char login[tam], int password) {
 			n = n->next;
 		}
 	}
+}
+
+int User_menu(User *u){
+	int option, password;
+	char login[tam];
+
+	scanf("%d", &option);
+	switch (option)
+	{
+	case 1:
+		printf("novo login: ");
+		scanf("%s", login);
+		printf("nova password: ");
+		scanf("%d", &password);
+		User_insert(u, login, password);
+		return 1;
+	case 2:
+		List_print(u);
+		return 2;
+	case 3:
+		printf("login que será deletado: ");
+		scanf("%s", login);
+		List_remove(u, login);
+		return 3;
+	case 4:
+		return 4;
+	default:
+		printf("\nDigite uma opção válida.\n");
+		return 0;
+	}
+}
+
+int main()
+{
+	User *u = User_alloc();
+	int i;
+	while (i != 4)
+	{	
+		printf("DIGITE O NÚMERO DA OPÇÃO DESEJADA\n");
+		printf("(1) CRIAR USUÁRIO.\n");
+		printf("(2) LISTAR USUÁRIOS.\n");
+		printf("(3) EXCLUIR USUÁRIO.\n");
+		printf("(4) SAIR DO PROGRAMA.\n");
+		printf("\n: ");
+		i = User_menu(u);
+	}
+	
+	return 0;
 }
--- arq/Listas/User.h
+++ arq/Listas/User.h
@@ -2,6 +2,7 @@ typedef struct User User;
 
 User *User_alloc();
 void User_free(User *u);
-void User_insert(User *l, char login[], int password);
-//LISTAR
-//REMOVER
+void User_insert(User *u, char login[], int password);
+void User_print(User *u);
+int User_remove(User* u, char login[]);
+int User_menu(User *u);
